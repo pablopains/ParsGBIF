544e5b6dba361528e884b8627d1e0c040b45d4cb
Pablo Hendrigo Alves de Melo <pablopains@yahoo.com.br>
2023-06-15 19:30:36 -0300

    readme update

---
+++ R/EnumOccurrenceIssue.R
@@ -0,0 +1,19 @@
+#' Enumeration GBIF issue
+#'
+#' An enumeration of validation rules for single occurrence records.
+#'
+#' @format ## `EnumOccurrenceIssue`
+#' A data frame with 69 rows and 9 columns:
+#' \describe{
+#'   \item{Constant}{GBIF Constant}
+#'   \item{Description}{Description}
+#'   \item{definition}{definition}
+#'   \item{type}{type}
+#'   \item{priority}{priority}
+#'   \item{score}{score}
+#'   \item{selection_score}{selection_score}
+#'   \item{reasoning}{reasoning}
+#'   \item{notes}{notes}
+#' }
+#' @source <https://gbif.github.io/parsers/apidocs/org/gbif/api/vocabulary/OccurrenceIssue.html>
+"EnumOccurrenceIssue"
